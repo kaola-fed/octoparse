0527701569f48ca60d6f019f15f56926c8f8f434
严晓波 <543024396@qq.com>
2018-09-14 16:23:19 +0800

    add plugin visitors feature

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,5 @@
 import octoParse from '../../lib/index.js';
+import removeBackground from '../../lib/plugins/removeBackground';
 Page({
   data: {
     name: 1
@@ -8,22 +9,24 @@ Page({
     /**
      * html解析示例
      */
-    var article = `<p class="test" id="mmm"><img src="http://haitao.nosdn2.127.net/onlineigirymdz12931.jpg" style="" /></p>
+    var article = `<p style="backGRound:red;border:10px solid gray;" class="test" id="mmm"><img src="http://haitao.nosdn2.127.net/onlineigirymdz12931.jpg" style="" /></p>
     <p class="test ddd"><img src="http://haitao.nosdn2.127.net/onlineigirymdz12931.jpg" style="" /></p>  
     <p class="test"><img src="http://haitao.nosdn1.127.net/ipz5qjmw83_710_341.jpg" /></p> 
+    <video src="http://v2v.cc/~j/theora_testsuite/320x240.ogg" controls>
+        你的浏览器不支持 <code>video</code> 标签.
+    </video>
     `;
     let options = {
-      p:{
-        include: '',
-        except: '',
-        removeAttrs: ['class'],
-        addAttrs: [{
-          name: 'style',
-          value: 'background:red;'
-        }]
+      platform: 'wechat',
+      plugins: [removeBackground],
+      visitors: {
+        p(node){
+          console.log('hello', node);
+        }
       }
     }
     let maxRes = octoParse.htmlParse(article, options);
+    console.log('解析结果是', maxRes);
     that.setData({
       article:maxRes
     })
