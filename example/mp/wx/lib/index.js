0af0a7cc08cc2b12d4263bac560e14f9963545a5
严晓波 <543024396@qq.com>
2018-10-29 10:08:11 +0800

    0.1.10

--- example/mp/wx/lib/index.js
+++
@@ -1,8 +0,0 @@
-import {htmlParse, install} from './octoParse';
-import mountAction from './mountAction';
-let octoparse = {
-    htmlParse,
-    install,
-    mountAction
-}
-export default octoparse;
