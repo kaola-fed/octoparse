bbacf0b8e5da0eb5783ba8e210a240d6e7f6e900
elcarim5efil <elcarim5efil@gmail.com>
2018-11-06 16:08:57 +0800

    feat: support swan

---
+++ example/mp/swan/pages/index/index.js
@@ -0,0 +1,68 @@
+import octoParse from '../../lib/index.js';
+Page({
+  data: {
+    name: 1
+  },
+  onLoad: function () {
+    var that = this;
+    /**
+     * html解析示例
+     */
+    var article = `<div class="wx_title">OCTOPATH TRAVELER</div>
+    <img src="http://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1540183348581&di=507d478e517ac2ef9472f89446239f7a&imgtype=0&src=http%3A%2F%2Fcms-bucket.nosdn.127.net%2F2018%2F07%2F18%2Faec8e5f0492247d78f49cdc3155c463e.jpeg" alt="octoparse">`;
+    let options = {
+      platform: 'wechat',
+      plugins: [],
+      visitors: {
+        img(node){
+          node.styleStr = '';
+        }
+      }
+    }
+    let maxRes = octoParse.htmlParse(article, options);
+    console.log('解析结果是', maxRes);
+    that.setData({
+      article:maxRes
+    })
+    octoParse.mountAction(that,[
+      {
+        tag:'img',
+        filter: '#img1',
+        events:{
+          tap: showImg
+        }
+      },
+      {
+        tag:'div',
+        filter: '.block',
+        events:{
+          tap: bindATagAction
+        }
+      },
+      {
+        tag:'a',
+        filter: '.testA',
+        events:{
+          tap: bindATagAction
+        }
+      },
+      {
+        tag:'a',
+        filter: '#testId',
+        events:{
+          tap: bindATagAction2
+        }
+      }
+    ]);
+    function showImg(e){
+      console.log('gg')
+    }
+    function bindATagAction(e){
+      console.log('hello yanxiaobo');
+    }
+    function bindATagAction2(e){
+      console.log('are u ok?');
+    }
+  }
+
+})
